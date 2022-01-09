8d1e52ffb846e33a2e1a6bfa244686286942c689
wangye <553311122@qq.com>
2022-01-10 00:27:59 +0800

    1

--- src/contracts/abi.js
+++ src/contracts/abi.js
@@ -13,13 +13,13 @@ const addrKey = [
 ]
 export const prodAddrs = fillZeroAddr({
   zuckToken: '0x03abf81d33e675b7df45ce832b1269cf7a4b3269',
-  zuckFactory:'0x3c8c49F40D37C68f3C81EEF5bd3bBa5993fDC9f4',
+  zuckFactory:'0x95eaCC67a9c4999c086905d42BC5d9aa4D7D2885',
   zuckNft:'0xA986230Cf9419eC0434A6AAE42Cf817850288Ae9'
 })
 
 export const testAddrs = fillZeroAddr({
   zuckToken: '0x03abf81d33e675b7df45ce832b1269cf7a4b3269',
-  zuckFactory:'0x3c8c49F40D37C68f3C81EEF5bd3bBa5993fDC9f4',
+  zuckFactory:'0x95eaCC67a9c4999c086905d42BC5d9aa4D7D2885',
   zuckNft:'0xA986230Cf9419eC0434A6AAE42Cf817850288Ae9'
 })
 function fillZeroAddr (addrs) {
